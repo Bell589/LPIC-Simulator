cff399745795bb965b9be042bf8c3f9894b73372
Marcel <marcel.lichte1@web.de>
2026-06-29 21:32:19 +0200

    git push

--- src/app/home/home-page/home-page.ts
+++ src/app/home/home-page/home-page.ts
@@ -1,21 +1,35 @@
-import { AsyncPipe } from '@angular/common';
-import { Component, inject } from '@angular/core';
-import { Observable } from 'rxjs';
-
-import { Question } from '../../core/models/question';
-import { QuestionDataService } from '../../core/services/question-data.service';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home-page',
-  imports: [AsyncPipe],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './home-page.html',
-  styleUrl: './home-page.css',
+  styleUrl: './home-page.css'
 })
 export class HomePage {
 
-  private readonly questionDataService = inject(QuestionDataService);
+  catalog = '101a';
 
-  protected readonly questions$: Observable<Question[]> =
-    this.questionDataService.getQuestions('101');
+  mode = 'practice';
+
+  order = 'normal';
+
+  questionType = 'all';
+
+  numberOfQuestions = 10;
+
+  startQuiz(): void {
+    console.log('Quiz starten');
+
+    console.log({
+      catalog: this.catalog,
+      mode: this.mode,
+      order: this.order,
+      questionType: this.questionType,
+      numberOfQuestions: this.numberOfQuestions
+    });
+  }
 
 }
