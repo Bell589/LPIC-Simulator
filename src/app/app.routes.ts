203f6b2bf9548ca8660f63e375a3ba76b4fa596d
Bell589 <marcel.lichte1@web.de>
2026-06-29 15:17:09 +0200

    1.3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ExamPage } from './exam/exam-page/exam-page';
+import { LearningPage } from './learn/learning-page/learning-page';
+import { QuestionListPage } from './question-list/question-list-page/question-list-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'exam', component: ExamPage },
+  { path: 'learn', component: LearningPage },
+  { path: 'questions', component: QuestionListPage },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
+];
